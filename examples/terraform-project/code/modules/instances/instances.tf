64f7f52ed57d89ec65f5a0700941d491304e50be
Hemant Sahu <119852186+hemant-techo@users.noreply.github.com>
2023-02-16 14:55:43 +0530

    Update instances.tf

--- examples/terraform-project/code/modules/instances/instances.tf
+++ examples/terraform-project/code/modules/instances/instances.tf
@@ -19,6 +19,27 @@ EOT
 allow_stopping_for_update = true
 }
 
+resource "google_compute_instance" "tf-instance-3" {
+  name         = "tf-instance-1"
+  machine_type = "n2d-highcpu-96"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+ network = "default"
+  }
+
+metadata_startup_script = <<-EOT
+#!/bin/bash
+EOT
+
+allow_stopping_for_update = true
+}
+
 resource "google_compute_instance" "tf-instance-2" {
   name         = "tf-instance-3"
   machine_type = "n2-standard-2"
