439213c588fe4a17252621f421172cd6d2e293fe
Hemant Sahu <119852186+hemant-techo@users.noreply.github.com>
2023-02-16 16:02:54 +0530

    Update main.tf

--- examples/terraform-project/code/main.tf
+++ examples/terraform-project/code/main.tf
@@ -27,10 +27,4 @@ terraform {
     bucket  = "<FILL IN PROJECT ID>"
  prefix  = "terraform/state"
   }
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "3.55.0"
-    }
-  }
 }
